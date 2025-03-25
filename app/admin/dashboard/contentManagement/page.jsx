6e3608213b348640ecf8c115766f3b59d1c136cb
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-03-26 01:23:38 +0300

    Part of code is removed

--- app/admin/dashboard/contentManagement/page.jsx
+++ app/admin/dashboard/contentManagement/page.jsx
@@ -1344,7 +1344,7 @@ const ContentManagement = () => {
                     </div>
 
                     {/* Süre - Yalnızca video içerikleri için gösteriliyor */}
-                    {(currentContent?.type === 'video' || formType === 'video') && (
+                    {/* {(currentContent?.type === 'video' || formType === 'video') && (
                       <div>
                         <label htmlFor="duration" className="block text-sm font-medium text-gray-700">
                           Süre (HH:MM:SS)
@@ -1359,7 +1359,7 @@ const ContentManagement = () => {
                           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         />
                       </div>
-                    )}
+                    )} */}
 
                     {/* Dosya Yükleme */}
                     <div>
