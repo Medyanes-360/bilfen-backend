97c52795c00d780a441f752183d5694b5392780c
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-03-25 13:58:47 +0300

    Style is changed

--- app/admin/dashboard/contentManagement/page.jsx
+++ app/admin/dashboard/contentManagement/page.jsx
@@ -888,7 +888,7 @@ const ContentManagement = () => {
                     Ek Materyal
                   </th>
                   <th scope="col" className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Eksik Yerler
+                    Yayın Kriterleri
                   </th>
                   <th scope="col" className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     İşlem
@@ -1240,7 +1240,7 @@ const ContentManagement = () => {
                           type="date"
                           name="studentPublishDate"
                           id="studentPublishDate"
-                          defaultValue={currentContent?.studentPublishDate || new Date().toISOString().split('T')[0]}
+                          defaultValue={currentContent?.studentPublishDate || ''}
                           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         />
                       </div>
@@ -1253,7 +1253,7 @@ const ContentManagement = () => {
                           type="date"
                           name="teacherPublishDate"
                           id="teacherPublishDate"
-                          defaultValue={currentContent?.teacherPublishDate || new Date().toISOString().split('T')[0]}
+                          defaultValue={currentContent?.teacherPublishDate || ''}
                           className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         />
                       </div>
