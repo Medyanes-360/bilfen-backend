8651e2212617e4ecc964020de8ab72462e2513b8
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-04-04 22:04:53 +0200

    Svg and mockdata are moved in component

---
+++ app/constants/mockData.js
@@ -0,0 +1,15 @@
+export const contentTypes = [
+    { value: "video", label: "Video" },
+    { value: "audio", label: "Ses" },
+    { value: "document", label: "Döküman" },
+    { value: "interactive", label: "Etkileşimli" },
+    { value: "game", label: "Oyun" }
+];
+
+export const ageGroups = [
+    { value: "3-4 yaş", label: "3-4 yaş" },
+    { value: "4-5 yaş", label: "4-5 yaş" },
+    { value: "5-6 yaş", label: "5-6 yaş" },
+    { value: "6-7 yaş", label: "6-7 yaş" },
+    { value: "7-8 yaş", label: "7-8 yaş" }
+];
