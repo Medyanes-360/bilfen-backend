99a9cb83bb708d5a20be30e5ad298e85265e57a1
Eda Işıl İnal <edainalisil@gmail.com>
2025-04-05 09:49:35 +0300

    replaced edit,deleteipublish,eye buttons

--- app/admin/dashboard/contentManagement/page.jsx
+++ app/admin/dashboard/contentManagement/page.jsx
@@ -1290,33 +1290,6 @@ const ContentManagement = () => {
                     </td>
                     <td className="px-3 py-2">
                       <div className="flex items-center space-x-1">
-                        <button
-                          onClick={() => viewContent(content.id)}
-                          className="text-indigo-600 hover:text-indigo-900"
-                        >
-                          <Eye className="w-4 h-4 cursor-pointer" />
-                        </button>
-                        <button
-                          onClick={() => openModal(content)}
-                          className="text-blue-600 hover:text-blue-900"
-                        >
-                          <Edit className="w-4 h-4 cursor-pointer" />
-                        </button>
-                        <button
-                          className="text-red-600 hover:text-red-900 cursor-pointer"
-                          title="Sil"
-                          onClick={() => {
-                            if (bulkMode && selectedItems.length > 1) {
-                              setBulkAction("delete");
-                              setBulkActionModalOpen(true);
-                            } else {
-                              setSelectedId(content.id);
-                              setConfirmOpen(true);
-                            }
-                          }}
-                        >
-                          <Trash2 className="w-5 h-5" />
-                        </button>
                         {(() => {
                           const missingFields = [];
                           const isWeekly = content.isWeeklyContent === true;
@@ -1339,13 +1312,13 @@ const ContentManagement = () => {
 
                           return (
                             <button
-                              className={`px-2 py-1 text-xs rounded-lg shadow-sm transition-all
-        ${isPublished
-                                  ? "bg-red-500 hover:bg-red-600 text-white cursor-pointer"
-                                  : isPublishDisabled
-                                    ? "bg-neutral-100 text-neutral-400 cursor-not-allowed border border-red-300"
-                                    : "bg-green-600 hover:bg-green-700 text-white cursor-pointer"
-                                }`}
+                            className={`w-[110px] h-[28px] px-2 py-1 text-xs rounded-lg shadow-sm transition-all
+                              ${isPublished
+                                ? "bg-red-500 hover:bg-red-600 text-white cursor-pointer"
+                                : isPublishDisabled
+                                  ? "bg-neutral-100 text-neutral-400 cursor-not-allowed border border-red-300"
+                                  : "bg-green-500 hover:bg-green-600 text-white cursor-pointer"
+                              }`}
                               disabled={!isPublished && isPublishDisabled}
                               title={
                                 isPublished
@@ -1366,6 +1339,33 @@ const ContentManagement = () => {
                             </button>
                           );
                         })()}
+                          <button
+                          onClick={() => viewContent(content.id)}
+                          className="text-blue-600 hover:text-blue-900"
+                        >
+                          <Eye className="w-4 h-4 cursor-pointer" />
+                        </button>
+                        <button
+                          onClick={() => openModal(content)}
+                          className="text-blue-600 hover:text-blue-900"
+                        >
+                          <Edit className="w-4 h-4 cursor-pointer" />
+                        </button>
+                           <button
+                          className="text-red-600 hover:text-red-900 cursor-pointer"
+                          title="Sil"
+                          onClick={() => {
+                            if (bulkMode && selectedItems.length > 1) {
+                              setBulkAction("delete");
+                              setBulkActionModalOpen(true);
+                            } else {
+                              setSelectedId(content.id);
+                              setConfirmOpen(true);
+                            }
+                          }}
+                        >
+                          <Trash2 className="w-5 h-5" />
+                        </button>
                       </div>
                     </td>
                   </tr>
