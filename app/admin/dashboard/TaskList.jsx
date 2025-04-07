af0149177835081480a68d6952b0ba9e8c4d73dc
Öykü Kaya <oykuky001@gmail.com>
2025-04-07 22:54:53 +0300

    to do completed and continued section update

--- app/admin/dashboard/TaskList.jsx
+++ app/admin/dashboard/TaskList.jsx
@@ -127,11 +127,16 @@ const TaskList = () => {
 
       if (editingTask) {
         // If editing, send a PATCH request
-        const updatedTask = await patchAPI(`/api/tasks/${editingTask.id}`, taskData);
+        const updatedTask = await patchAPI(
+          `/api/tasks/${editingTask.id}`,
+          taskData
+        );
         if (updatedTask !== null) {
           // Check if updatedTask is not null
           setTasks((prevTasks) =>
-            prevTasks.map((task) => (task.id === updatedTask.id ? updatedTask : task))
+            prevTasks.map((task) =>
+              task.id === updatedTask.id ? updatedTask : task
+            )
           );
         } else {
           // If updatedTask is null, refetch tasks to ensure the list is updated
@@ -223,7 +228,8 @@ const TaskList = () => {
           <h3 className="text-lg font-medium text-gray-900">Görevler</h3>
           <button
             onClick={openNewTaskModal}
-            className="inline-flex items-center px-3 py-1.5 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+            className="inline-flex items-center px-3 py-1.5 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+          >
             <PlusCircle size={16} className="mr-1" />
             Yeni Görev
           </button>
@@ -237,10 +243,12 @@ const TaskList = () => {
               <button
                 key={button.value}
                 onClick={() => setActiveFilter(button.value)}
-                className={`px-3 py-1.5 text-xs font-medium rounded-md ${activeFilter === button.value
-                  ? "bg-indigo-100 text-indigo-700"
-                  : "text-gray-700 hover:bg-gray-100"
-                  }`}>
+                className={`px-3 py-1.5 text-xs font-medium rounded-md ${
+                  activeFilter === button.value
+                    ? "bg-indigo-100 text-indigo-700"
+                    : "text-gray-700 hover:bg-gray-100"
+                }`}
+              >
                 {button.label}
               </button>
             ))}
@@ -253,7 +261,10 @@ const TaskList = () => {
               onChange={() => setShowCompleted(!showCompleted)}
               className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
             />
-            <label htmlFor="showCompleted" className="ml-2 text-sm text-gray-700">
+            <label
+              htmlFor="showCompleted"
+              className="ml-2 text-sm text-gray-700"
+            >
               Tamamlananları göster
             </label>
           </div>
@@ -268,10 +279,12 @@ const TaskList = () => {
                 <div className="flex-shrink-0 pt-1">
                   <button
                     onClick={() => toggleTaskStatus(task.id)}
-                    className={`w-5 h-5 rounded-full border flex items-center justify-center ${task?.status === "Tamamlandı"
-                      ? "bg-green-500 border-green-500 text-white"
-                      : "border-gray-400"
-                      }`}>
+                    className={`w-5 h-5 rounded-full border flex items-center justify-center ${
+                      task?.status === "Tamamlandı"
+                        ? "bg-green-500 border-green-500 text-white"
+                        : "border-gray-400"
+                    }`}
+                  >
                     {task?.status === "Tamamlandı" && <Check size={12} />}
                   </button>
                 </div>
@@ -279,12 +292,15 @@ const TaskList = () => {
                   <div className="flex items-center justify-between">
                     <div
                       onClick={() => toggleExpand(task.id)}
-                      className="flex items-center cursor-pointer">
+                      className="flex items-center cursor-pointer"
+                    >
                       <h4
-                        className={`text-sm font-medium ${task.status === "Tamamlandı"
-                          ? "text-gray-500 line-through"
-                          : "text-gray-900"
-                          }`}>
+                        className={`text-sm font-medium ${
+                          task.status === "Tamamlandı"
+                            ? "text-gray-500 line-through"
+                            : "text-gray-900"
+                        }`}
+                      >
                         {task?.title}
                       </h4>
                       {task?.expanded ? (
@@ -297,13 +313,15 @@ const TaskList = () => {
                       <span
                         className={`px-2 py-1 text-xs font-medium rounded-full flex items-center ${getPriorityClass(
                           task.priority
-                        )}`}>
+                        )}`}
+                      >
                         {task?.priority}
                       </span>
                       <span
                         className={`px-2 py-1 text-xs font-medium rounded-full flex items-center ${getStatusClass(
                           task?.status
-                        )}`}>
+                        )}`}
+                      >
                         {getStatusIcon(task.status)}
                         <span className="ml-1">{task?.status}</span>
                       </span>
@@ -312,7 +330,9 @@ const TaskList = () => {
 
                   {task.expanded && (
                     <div className="mt-2">
-                      <p className="text-sm text-gray-600">{task.description}</p>
+                      <p className="text-sm text-gray-600">
+                        {task.description}
+                      </p>
                       <div className="mt-2 flex items-center text-xs text-gray-500">
                         <Calendar size={14} className="mr-1" />
                         <span>Son Tarih: {task.dueDate}</span>
@@ -320,13 +340,15 @@ const TaskList = () => {
                       <div className="mt-3 flex space-x-2">
                         <button
                           onClick={() => editTask(task)}
-                          className="inline-flex items-center px-2 py-1 text-xs font-medium rounded border border-gray-300 text-gray-700 bg-white hover:bg-gray-50">
+                          className="inline-flex items-center px-2 py-1 text-xs font-medium rounded border border-gray-300 text-gray-700 bg-white hover:bg-gray-50"
+                        >
                           <Edit size={12} className="mr-1" />
                           Düzenle
                         </button>
                         <button
                           onClick={() => deleteTask(task.id)}
-                          className="inline-flex items-center px-2 py-1 text-xs font-medium rounded border border-red-300 text-red-700 bg-white hover:bg-red-50">
+                          className="inline-flex items-center px-2 py-1 text-xs font-medium rounded border border-red-300 text-red-700 bg-white hover:bg-red-50"
+                        >
                           <Trash2 size={12} className="mr-1" />
                           Sil
                         </button>
@@ -344,13 +366,23 @@ const TaskList = () => {
         )}
       </ul>
 
-      <div className="text-sm text-gray-500">
-        <span className="font-medium">
-          {Array.isArray(tasks) ? tasks.filter(t => t.status === 'Tamamlandı').length : 0}
-        </span> tamamlandı,{' '}
-        <span className="font-medium">
-          {Array.isArray(tasks) ? tasks.filter(t => t.status !== 'Tamamlandı').length : 0}
-        </span> devam ediyor
+      <div className="ml-4 pb-2 flex gap-1 text-sm text-gray-500">
+        <div className="p-1 rounded-full bg-green-100 text-green-800">
+          <span className="font-medium w-fit px-2">
+            {Array.isArray(tasks)
+              ? tasks.filter((t) => t.status === "Tamamlandı").length
+              : 0}
+          </span>{" "}
+          tamamlandı{" "}
+        </div>
+        <div className="p-1 rounded-full bg-blue-100 text-blue-800">
+          <span className="font-medium w-fit px-2">
+            {Array.isArray(tasks)
+              ? tasks.filter((t) => t.status !== "Tamamlandı").length
+              : 0}
+          </span>{" "}
+          devam ediyor
+        </div>
       </div>
       {/* Görev Ekleme/Düzenleme Modal */}
       {showTaskModal && (
@@ -362,7 +394,8 @@ const TaskList = () => {
               </h3>
               <button
                 onClick={() => setShowTaskModal(false)}
-                className="text-gray-400 hover:text-gray-500">
+                className="text-gray-400 hover:text-gray-500"
+              >
                 <X size={20} />
               </button>
             </div>
@@ -370,7 +403,10 @@ const TaskList = () => {
             <form onSubmit={handleSubmit}>
               <div className="space-y-4">
                 <div>
-                  <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Başlık
                   </label>
                   <input
@@ -385,7 +421,10 @@ const TaskList = () => {
                 </div>
 
                 <div>
-                  <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="description"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Açıklama
                   </label>
                   <textarea
@@ -394,12 +433,16 @@ const TaskList = () => {
                     rows="3"
                     value={taskForm.description}
                     onChange={handleInputChange}
-                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  ></textarea>
                 </div>
 
                 <div className="grid grid-cols-2 gap-4">
                   <div>
-                    <label htmlFor="dueDate" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="dueDate"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Son Tarih
                     </label>
                     <input
@@ -414,7 +457,10 @@ const TaskList = () => {
                   </div>
 
                   <div>
-                    <label htmlFor="priority" className="block text-sm font-medium text-gray-700">
+                    <label
+                      htmlFor="priority"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Öncelik
                     </label>
                     <select
@@ -423,7 +469,8 @@ const TaskList = () => {
                       required
                       value={taskForm.priority}
                       onChange={handleInputChange}
-                      className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                      className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
                       <option value="Düşük">Düşük</option>
                       <option value="Orta">Orta</option>
                       <option value="Yüksek">Yüksek</option>
@@ -432,7 +479,10 @@ const TaskList = () => {
                 </div>
 
                 <div>
-                  <label htmlFor="status" className="block text-sm font-medium text-gray-700">
+                  <label
+                    htmlFor="status"
+                    className="block text-sm font-medium text-gray-700"
+                  >
                     Durum
                   </label>
                   <select
@@ -441,7 +491,8 @@ const TaskList = () => {
                     required
                     value={taskForm.status}
                     onChange={handleInputChange}
-                    className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    className="mt-1 block w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
                     <option value="Beklemede">Beklemede</option>
                     <option value="Devam Ediyor">Devam Ediyor</option>
                     <option value="Tamamlandı">Tamamlandı</option>
@@ -452,12 +503,14 @@ const TaskList = () => {
                   <button
                     type="button"
                     onClick={() => setShowTaskModal(false)}
-                    className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+                    className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                  >
                     İptal
                   </button>
                   <button
                     type="submit"
-                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
                     {editingTask ? "Güncelle" : "Ekle"}
                   </button>
                 </div>
