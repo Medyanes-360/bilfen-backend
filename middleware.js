345d3b2930f1495cc6b088acd67216ad130c55ac
ozudev <ozudev@gmail.com>
2025-04-10 09:05:07 +0300

    fix

--- middleware.js
+++ middleware.js
@@ -4,9 +4,12 @@ export function middleware(request) {
   const response = NextResponse.next();
 
   // CORS headers
-  response.headers.set("Access-Control-Allow-Origin", "https://bilfen-frontend.vercel.app");
+  response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
   response.headers.set("Access-Control-Allow-Credentials", "true");
 
   // OPTIONS request için özel yanıt
